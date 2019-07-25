a84b3e219f04dcf7040830f866480ac88b6acc4f
Minori Hashimoto <minori.fh@gmail.com>
2019-07-25 10:26:43 -0700

    add jquery to manipulate border bottom css on click of menu items

--- site.js
+++ site.js
@@ -6,23 +6,32 @@ $(document).ready(function() {
     
     $(".how").on("click", function(){
         $("#how-section").show();
+        $(".how").css("border-bottom", "solid white 1px")
 
         $("#why-section").hide();
         $("#what-section").hide();
+        $(".why").css("border-bottom", "none")
+        $(".what").css("border-bottom", "none")
     });
     
     $(".why").on("click", function(){
         $("#why-section").show();
+        $(".why").css("border-bottom", "solid white 1px")
 
         $("#how-section").hide();
         $("#what-section").hide();
+        $(".how").css("border-bottom", "none")
+        $(".what").css("border-bottom", "none")
     
     });
     
     $(".what").on("click", function(){
         $("#what-section").show();
+        $(".what").css("border-bottom", "solid white 1px")
 
         $("#how-section").hide();
         $("#why-section").hide();
+        $(".how").css("border-bottom", "none")
+        $(".why").css("border-bottom", "none")
     });
 });
