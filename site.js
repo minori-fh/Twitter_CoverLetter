d03542ed35d36a055954a252c6aee82ca936748c
Minori Hashimoto <minori.fh@gmail.com>
2019-07-25 16:36:02 -0700

    add comments to make code readable

--- site.js
+++ site.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-
+    // Hide what and why sections on page load
     $("#what-section").hide();
     $("#why-section").hide();
     
-    
+    // Event handler for when user clicks on "how i got here"
     $(".how").on("click", function(){
         $("#how-section").show();
+        // Change color of active menu item
         $(".how").css("color", "black")
 
         $("#why-section").hide();
@@ -13,9 +14,23 @@ $(document).ready(function() {
         $(".why").css("color", "white")
         $(".what").css("color", "white")
     });
+
+    // Event handler for when user clicks on "what sets me apart"
+    $(".what").on("click", function(){
+        $("#what-section").show();
+        // Change color of active menu item
+        $(".what").css("color", "black")
+
+        $("#how-section").hide();
+        $("#why-section").hide();
+        $(".how").css("color", "white")
+        $(".why").css("color", "white")
+    });
     
+    // Event handler for when user clicks on "why I want this apprenticeship"
     $(".why").on("click", function(){
         $("#why-section").show();
+        // Change color of active menu item
         $(".why").css("color", "black")
 
         $("#how-section").hide();
@@ -24,13 +39,4 @@ $(document).ready(function() {
         $(".what").css("color", "white")
     });
     
-    $(".what").on("click", function(){
-        $("#what-section").show();
-        $(".what").css("color", "black")
-
-        $("#how-section").hide();
-        $("#why-section").hide();
-        $(".how").css("color", "white")
-        $(".why").css("color", "white")
-    });
 });
